b60f831cc1ef4b3c824b07a0a738f0955236e62c
Anatolii Shablov <anatoliishablov@gmail.com>
2020-01-18 01:50:47 +0300

    to_string added + refactor

--- include/address_v4.h
+++ include/address_v4.h
@@ -1,5 +1,5 @@
-#ifndef ADDRESS_V4_H
-#define ADDRESS_V4_H
+#ifndef AIOS_ADDRESS_V4_H
+#define AIOS_ADDRESS_V4_H
 
 #include <array>
 #include <cstdint>
@@ -7,16 +7,17 @@
 #include "endian.h"
 #include "error_code.h"
 
-namespace aios {
-namespace ip {
+namespace aios::ip {
 class address_v4 {
 public:
   using uint_type = uint32_t;
   using bytes_type = std::array<uint8_t, 4>;
 
   constexpr address_v4() noexcept : data_{0} {}
+
   constexpr explicit address_v4(uint_type addr) noexcept
       : data_{aios::system::host_to_network(addr)} {}
+
   constexpr explicit address_v4(bytes_type const &bytes) noexcept
       : data_{aios::system::host_to_network(
             static_cast<uint32_t>(bytes[0]) << UINT32_C(24) |
@@ -24,24 +25,58 @@ public:
             static_cast<uint32_t>(bytes[2]) << UINT32_C(8) |
             static_cast<uint32_t>(bytes[3]) << UINT32_C(0))} {}
 
-  constexpr address_v4(address_v4 const &other) noexcept : data_{other.data_} {}
+  constexpr address_v4(address_v4 const &other) noexcept = default;
 
-  constexpr address_v4 &operator=(address_v4 const &other) noexcept {
-    data_ = other.data_;
-    return *this;
+  constexpr explicit address_v4(std::string_view str) noexcept : data_{0} {
+    if (str == "localhost") {
+      *this = loopback();
+      return;
+    }
+    std::array<uint8_t, 4> bytes{UINT8_C(0), UINT8_C(0), UINT8_C(0),
+                                 UINT8_C(0)};
+    uint32_t i{0};
+    uint32_t tmp{0};
+    for (char c : str) {
+      if (tmp > UINT32_C(255)) {
+        return;
+      }
+      if (c == '.') {
+        bytes[i] = static_cast<uint8_t>(tmp);
+        if (++i > UINT32_C(3)) {
+          return;
+        }
+      } else if (c > '0' && c < '9') {
+        tmp *= UINT32_C(10);
+        tmp += static_cast<uint32_t>(c - '0');
+      } else {
+        return;
+      }
+    }
+    if (tmp > UINT32_C(255)) {
+      return;
+    } else {
+      bytes[3] = static_cast<uint8_t>(tmp);
+    }
+    *this = address_v4{bytes};
   }
 
+  address_v4(std::string_view str, aios::system::error_code &ec) noexcept;
+
+  constexpr address_v4 &operator=(address_v4 const &other) noexcept = default;
+
   [[nodiscard]] constexpr bool is_loopback() const noexcept {
-    return (aios::system::network_to_host(data_) > UINT32_C(0x7f000001)) &&
-           (aios::system::network_to_host(data_) < UINT32_C(0x7fffffff));
+    return (aios::system::network_to_host(data_) >= UINT32_C(0x7f000001)) &&
+           (aios::system::network_to_host(data_) <= UINT32_C(0x7fffffff));
   }
 
   [[nodiscard]] constexpr bool is_multicast() const noexcept {
     return aios::system::network_to_host(data_) & UINT32_C(0xE0000000);
   }
+
   [[nodiscard]] constexpr bool is_unspecified() const noexcept {
     return data_ == UINT32_C(0);
   }
+
   [[nodiscard]] constexpr bytes_type to_bytes() const noexcept {
     uint32_t host_variant = aios::system::network_to_host(data_);
     return bytes_type{static_cast<uint8_t>(host_variant >> UINT32_C(24)),
@@ -53,15 +88,19 @@ public:
   [[nodiscard]] constexpr uint_type to_host_uint() const noexcept {
     return aios::system::network_to_host(data_);
   }
+
   [[nodiscard]] constexpr uint_type to_network_uint() const noexcept {
     return data_;
   }
+
   [[nodiscard]] static constexpr address_v4 any() noexcept {
     return address_v4{};
   }
+
   [[nodiscard]] static constexpr address_v4 loopback() noexcept {
     return address_v4{UINT32_C(0x7f000001)};
   }
+
   [[nodiscard]] friend constexpr bool
   operator==(address_v4 const &lhs, address_v4 const &rhs) noexcept {
     return lhs.data_ == rhs.data_;
@@ -72,95 +111,11 @@ public:
     return lhs.data_ != rhs.data_;
   }
 
+  [[nodiscard]] std::string to_string();
+
 private:
   uint_type data_;
 };
-
-[[nodiscard]] constexpr address_v4
-make_address_v4(address_v4::bytes_type const &bytes) noexcept {
-  return address_v4{bytes};
-}
-[[nodiscard]] constexpr address_v4
-make_address_v4(address_v4::uint_type addr) noexcept {
-  return address_v4{addr};
-}
-
-[[nodiscard]] address_v4 make_address_v4(std::string const &str) noexcept;
-[[nodiscard]] address_v4 make_address_v4(std::string const &str,
-                                         aios::system::error_code &ec) noexcept;
-
-[[nodiscard]] constexpr address_v4
-make_address_v4(std::string_view str) noexcept {
-  if (str == "localhost") {
-    return address_v4::loopback();
-  }
-  std::array<uint8_t, 4> bytes{UINT8_C(0), UINT8_C(0), UINT8_C(0), UINT8_C(0)};
-  uint32_t i{0};
-  uint32_t tmp{0};
-  for (char c : str) {
-    if (tmp > UINT32_C(255)) {
-      return address_v4::any();
-    }
-    if (c == '.') {
-      bytes[i] = static_cast<uint8_t>(tmp);
-      if (++i > UINT32_C(3)) {
-        return address_v4::any();
-      }
-    } else if (c > '0' && c < '9') {
-      tmp *= UINT32_C(10);
-      tmp += static_cast<uint32_t>(c - '0');
-    } else {
-      return address_v4::any();
-    }
-  }
-  if (tmp > UINT32_C(255)) {
-    return address_v4::any();
-  } else {
-    bytes[3] = static_cast<uint8_t>(tmp);
-  }
-  return address_v4{bytes};
-}
-
-[[nodiscard]] address_v4 make_address_v4(std::string_view str,
-                                         aios::system::error_code &ec) noexcept;
-
-[[nodiscard]] constexpr address_v4 make_address_v4(char const *str) noexcept {
-  if (str[0] == 'l' && str[1] == 'o' && str[2] == 'c' && str[3] == 'a' &&
-      str[4] == 'l' && str[5] == 'h' && str[6] == 'o' && str[7] == 's' &&
-      str[8] == 't' && str[9] == '\0') {
-    return address_v4::loopback();
-  }
-  std::array<uint8_t, 4> bytes{UINT8_C(0), UINT8_C(0), UINT8_C(0), UINT8_C(0)};
-  uint32_t i{0};
-  uint32_t tmp{0};
-  for (size_t j = 0; str[j] != '\0'; ++j) {
-    char const &c = str[j];
-    if (tmp > UINT32_C(255)) {
-      return address_v4::any();
-    }
-    if (c == '.') {
-      bytes[i] = static_cast<uint8_t>(tmp);
-      if (++i > UINT32_C(3)) {
-        return address_v4::any();
-      }
-    } else if (c > '0' && c < '9') {
-      tmp *= UINT32_C(10);
-      tmp += static_cast<uint32_t>(c - '0');
-    } else {
-      return address_v4::any();
-    }
-  }
-  if (tmp > UINT32_C(255)) {
-    return address_v4::any();
-  } else {
-    bytes[3] = static_cast<uint8_t>(tmp);
-  }
-  return address_v4{bytes};
-}
-
-[[nodiscard]] address_v4 make_address_v4(char const *str,
-                                         aios::system::error_code &ec) noexcept;
-} // namespace ip
-} // namespace aios
+} // namespace aios::ip
 
 #endif // ADDRESS_V4_H
--- src/address_v4.cpp
+++ src/address_v4.cpp
@@ -1,144 +1,45 @@
 #include "../include/address_v4.h"
 
-aios::ip::address_v4
-aios::ip::make_address_v4(const std::string &str) noexcept {
+aios::ip::address_v4::address_v4(std::string_view str,
+                                 aios::system::error_code &ec) noexcept
+    : data_{0} {
   if (str == "localhost") {
-    return address_v4::loopback();
+    ec = aios::system::error_code::no_errors;
+    *this = loopback();
+    return;
   }
+  ec = aios::system::error_code::bad_address_convertation;
   std::array<uint8_t, 4> bytes{UINT8_C(0), UINT8_C(0), UINT8_C(0), UINT8_C(0)};
   uint32_t i{0};
   uint32_t tmp{0};
   for (char c : str) {
     if (tmp > UINT32_C(255)) {
-      return address_v4::any();
+      return;
     }
     if (c == '.') {
       bytes[i] = static_cast<uint8_t>(tmp);
       if (++i > UINT32_C(3)) {
-        return address_v4::any();
+        return;
       }
     } else if (c > '0' && c < '9') {
       tmp *= UINT32_C(10);
       tmp += static_cast<uint32_t>(c - '0');
     } else {
-      return address_v4::any();
+      return;
     }
   }
   if (tmp > UINT32_C(255)) {
-    return address_v4::any();
+    return;
   } else {
     bytes[3] = static_cast<uint8_t>(tmp);
   }
-  return address_v4{bytes};
+  ec = aios::system::error_code::no_errors;
+  *this = address_v4(bytes);
 }
 
-aios::ip::address_v4
-aios::ip::make_address_v4(const std::string &str,
-                          aios::system::error_code &ec) noexcept {
-  if (str == "localhost") {
-    ec = aios::system::error_code::NO_ERRORS;
-    return address_v4::loopback();
-  }
-  ec = aios::system::error_code::BAD_ADDRESS_CONVERTATION;
-  std::array<uint8_t, 4> bytes{UINT8_C(0), UINT8_C(0), UINT8_C(0), UINT8_C(0)};
-  uint32_t i{0};
-  uint32_t tmp{0};
-  for (char c : str) {
-    if (tmp > UINT32_C(255)) {
-      return address_v4::any();
-    }
-    if (c == '.') {
-      bytes[i] = static_cast<uint8_t>(tmp);
-      if (++i > UINT32_C(3)) {
-        return address_v4::any();
-      }
-    } else if (c > '0' && c < '9') {
-      tmp *= UINT32_C(10);
-      tmp += static_cast<uint32_t>(c - '0');
-    } else {
-      return address_v4::any();
-    }
-  }
-  if (tmp > UINT32_C(255)) {
-    return address_v4::any();
-  } else {
-    bytes[3] = static_cast<uint8_t>(tmp);
-  }
-  ec = aios::system::error_code::NO_ERRORS;
-  return address_v4{bytes};
-}
-
-aios::ip::address_v4
-aios::ip::make_address_v4(std::string_view str,
-                          aios::system::error_code &ec) noexcept {
-  if (str == "localhost") {
-    ec = aios::system::error_code::NO_ERRORS;
-    return address_v4::loopback();
-  }
-  ec = aios::system::error_code::BAD_ADDRESS_CONVERTATION;
-  std::array<uint8_t, 4> bytes{UINT8_C(0), UINT8_C(0), UINT8_C(0), UINT8_C(0)};
-  uint32_t i{0};
-  uint32_t tmp{0};
-  for (char c : str) {
-    if (tmp > UINT32_C(255)) {
-      return address_v4::any();
-    }
-    if (c == '.') {
-      bytes[i] = static_cast<uint8_t>(tmp);
-      if (++i > UINT32_C(3)) {
-        return address_v4::any();
-      }
-    } else if (c > '0' && c < '9') {
-      tmp *= UINT32_C(10);
-      tmp += static_cast<uint32_t>(c - '0');
-    } else {
-      return address_v4::any();
-    }
-  }
-  if (tmp > UINT32_C(255)) {
-    return address_v4::any();
-  } else {
-    bytes[3] = static_cast<uint8_t>(tmp);
-  }
-  ec = aios::system::error_code::NO_ERRORS;
-  return address_v4{bytes};
-}
-
-aios::ip::address_v4
-aios::ip::make_address_v4(const char *str,
-                          aios::system::error_code &ec) noexcept {
-  if (str[0] == 'l' && str[1] == 'o' && str[2] == 'c' && str[3] == 'a' &&
-      str[4] == 'l' && str[5] == 'h' && str[6] == 'o' && str[7] == 's' &&
-      str[8] == 't' && str[9] == '\0') {
-    ec = aios::system::error_code::NO_ERRORS;
-    return address_v4::loopback();
-  }
-  std::array<uint8_t, 4> bytes{UINT8_C(0), UINT8_C(0), UINT8_C(0), UINT8_C(0)};
-  uint32_t i{0};
-  uint32_t tmp{0};
-  ec = aios::system::error_code::BAD_ADDRESS_CONVERTATION;
-  for (size_t j = 0; str[j] != '\0'; ++j) {
-    char const &c = str[j];
-    if (tmp > UINT32_C(255)) {
-      return address_v4::any();
-    }
-    if (c == '.') {
-      bytes[i] = static_cast<uint8_t>(tmp);
-      if (++i > UINT32_C(3)) {
-        return address_v4::any();
-      }
-    } else if (c > '0' && c < '9') {
-      tmp *= UINT32_C(10);
-      tmp += static_cast<uint32_t>(c - '0');
-    } else {
-      return address_v4::any();
-    }
-  }
-  if (tmp > UINT32_C(255)) {
-    return address_v4::any();
-  } else {
-    bytes[3] = static_cast<uint8_t>(tmp);
-  }
-  ec = aios::system::error_code::NO_ERRORS;
-  return address_v4{bytes};
+std::string aios::ip::address_v4::to_string() {
+  return std::to_string(static_cast<uint32_t>(to_bytes()[0])) + '.' +
+         std::to_string(static_cast<uint32_t>(to_bytes()[1])) + '.' +
+         std::to_string(static_cast<uint32_t>(to_bytes()[2])) + '.' +
+         std::to_string(static_cast<uint32_t>(to_bytes()[3]));
 }
