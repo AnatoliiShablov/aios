4ac6c928928559f8bc58b6c1c3b2799fc746836a
Anatolii Shablov <anatoliishablov@gmail.com>
2020-01-18 01:53:19 +0300

    one lib file

---
+++ include/aios.h
@@ -0,0 +1,14 @@
+#ifndef AIOS_AIOS_H
+#define AIOS_AIOS_H
+
+#include "address_v4.h"
+#include "endian.h"
+#include "endpoint.h"
+#include "error_code.h"
+#include "io_processor.h"
+#include "tcp_acceptor.h"
+#include "tcp_socket.h"
+#include "timer.h"
+#include "timer_processor.h"
+
+#endif // AIOS_AIOS_H
